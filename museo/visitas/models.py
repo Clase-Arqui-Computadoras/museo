b82bd81cd280e58754a5057e2b2acc1716ddadf1
Rafael <perezaguirrerafael@gmail.com>
2021-03-24 16:14:49 -0600

    [UPDATE] auto_now_add timestamp_in

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Visita(models.Model):
     email = models.EmailField()
-    timestamp_in = models.DateTimeField()
-    timestamp_out = models.DateTimeField()
-    comment = models.CharField(max_length=140)
+    timestamp_in = models.DateTimeField(auto_now_add=True)
+    timestamp_out = models.DateTimeField(blank=True, null=True)
+    comment = models.CharField(max_length=140, blank=True)
