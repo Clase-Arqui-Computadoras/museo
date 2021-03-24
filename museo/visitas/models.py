428f09ca24e470fd7849e459dd1b7798f9f02f63
Rafael <perezaguirrerafael@gmail.com>
2021-03-24 14:39:33 -0600

    [FIX] model Visita

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Visita(models.Model):
+    email = models.EmailField()
+    timestamp_in = models.DateTimeField()
+    timestamp_out = models.DateTimeField()
+    comment = models.CharField(max_length=140)
